54cd847fc8fb8206c0cc31c5a211edf57f1686b9
fengyuanyuan <fengyuanyuan@10.232.146.8>
2025-04-24 02:37:16 +0800

    Assignment 9

--- src/assignment9/BodySegment.java
+++ src/assignment9/BodySegment.java
@@ -6,20 +6,35 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class BodySegment {
 
-	private double x, y, size;
-	private Color color;
-	
-	public BodySegment(double x, double y, double size) {
-		//FIXME
-		
-		//See ColorUtils for some color options (or choose your own)
-	}
-	
-	/**
-	 * Draws the segment
-	 */
-	public void draw() {
-		//FIXME
-	}
-	
+ private double x, y, size;
+ private Color color;
+ 
+ public BodySegment(double x, double y, double size) {
+  //FIXME
+  this.x = x;
+  this.y = y;
+  this.size = size;
+
+  this.color = color.BLUE;
+  //See ColorUtils for some color options (or choose your own)
+ }
+ 
+
+ /**
+  * Draws the segment
+  */
+ public void draw() {
+  StdDraw.setPenColor(color);
+  StdDraw.filledCircle(this.x, this.y,this.size);
+
+ }
+ 
+ public double getX() {
+     return x;
+ }
+
+ public double getY() {
+     return y;
+ }
+ 
 }
--- src/assignment9/Food.java
+++ src/assignment9/Food.java
@@ -6,21 +6,30 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Food {
 
-	public static final double FOOD_SIZE = 0.02;
-	private double x, y;
-	
-	/**
-	 * Creates a new Food at a random location
-	 */
-	public Food() {
-		//FIXME
-	}
-	
-	/**
-	 * Draws the Food
-	 */
-	public void draw() {
-		//FIXME
-	}
-	
+ public static final double FOOD_SIZE = 0.02;
+ private double x, y;
+ 
+ /**
+  * Creates a new Food at a random location
+  */
+ public Food() {
+  this.x = Math.random();
+  this.y = Math.random();
+ }
+ 
+ public double getX() {
+  return this.x;
+ }
+ 
+ public double getY() {
+  return this.y;
+ }
+ /**
+  * Draws the Food
+  */
+ public void draw() {
+  StdDraw.setPenColor(Color.GREEN);
+  StdDraw.filledCircle(x, y, FOOD_SIZE);
+ }
+ 
 }
